d5c35085049e05ac8385bc8e483b315cff5b86af
Defoiaif <94846680+Defoiaif@users.noreply.github.com>
2024-07-04 17:26:57 +0200

    Modify classes achitecture

--- core/src/main/scala/DirectedGraph.scala
+++ core/src/main/scala/DirectedGraph.scala
@@ -1,10 +1,3 @@
 case class DirectedGraph[V](adjacencyList: Map[V, Map[V, Long]]) extends Graph[V, DirectedGraph[V]](adjacencyList) {
   override protected def newGraph(adjacencyList: Map[V, Map[V, Long]]): DirectedGraph[V] = DirectedGraph(adjacencyList)
-
-  override def getAllEdges: Set[Tuple] =
-    adjacencyList.flatMap { case (v, neighbors) => 
-      neighbors.map { case (n, e) => (v, n) } 
-    }.toSet
-
-  override def addEdge(source: V, destination: V, edge: Long = 1): DirectedGraph[V] = super.addEdge(source, destination)
 }
