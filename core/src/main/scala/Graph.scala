deb395983d5a4a90e1c0dd8a8782f656fe96c763
Defoiaif <94846680+Defoiaif@users.noreply.github.com>
2024-07-04 16:43:47 +0200

    Implement inheritance

--- core/src/main/scala/Graph.scala
+++ core/src/main/scala/Graph.scala
@@ -1,7 +1,24 @@
-trait Graph[V, E] {
-  def vertices: Set[V]
-  def edges: Set[(V, V, E)]
-  def neighbors(vertex: V): Set[V]
-  def addEdge(source: V, destination: V, edge: E): Graph[V, E]
-  def removeEdge(source: V, destination: V): Graph[V, E]
+abstract class Graph[V, G <: Graph[V, G]] (adjacencyList: Map[V, Map[V, Long]]) {
+  self: G =>
+
+  def getAllVertices: Set[V] = adjacencyList.keySet
+
+  def getAllEdges: Set[Tuple]
+
+  def getNeighbors(vertex: V): Set[V] = 
+    adjacencyList.getOrElse(vertex, Map.empty).keySet
+  
+  def addEdge(source: V, destination: V, edge: Long = 1): G = {
+    val neighbors = adjacencyList.getOrElse(source, Map.empty)
+    val updatedNeighbors = neighbors + (destination -> edge)
+    newGraph(adjacencyList + (source -> updatedNeighbors))
+  }
+
+  def removeEdge(source: V, destination: V): G = {
+    val neighbors = adjacencyList.getOrElse(source, Map.empty)
+    val updatedNeighbors = neighbors - destination
+    newGraph(adjacencyList + (source -> updatedNeighbors))
+  }
+
+  protected def newGraph(adjacencyList: Map[V, Map[V, Long]]): G
 }
